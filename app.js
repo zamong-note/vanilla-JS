4e9b4a4b28f1e11de365df420a18b781ecb7184b
zamong-note <tamongnote@naver.com>
2026-05-02 19:58:06 +0900

    #3.5 More Events

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const title = document.querySelector("div.title:first-child h1");
+const h1 = document.querySelector("div.title:first-child h1");
 
 function handleTitleClick() {
 	console.log("title was clicked!");
-    title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
 function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
+    h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);  /// 클릭하는걸 리스닝하고 있음. 발생하면 function이 동작하게 한다. 
-title.addEventListener("mouseenter", handleMouseEnter);   /// 마우스가 올라갔을 때
-title.addEventListener("mouseleave", handleMouseLeave);   /// 마우스가 떠났을 때
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
 
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("No wifi");
+}
+
+function handleWindowOnline() {
+    alert("Good wifi");
+}
+
+h1.addEventListener("click", handleTitleClick);  /// title.onclick = handleTitleClick; 이렇게 쓸 수도 있다
+h1.addEventListener("mouseenter", handleMouseEnter);   /// 마우스가 올라갔을 때
+h1.addEventListener("mouseleave", handleMouseLeave);   /// 마우스가 떠났을 때
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);   /// wifi 꺼졌을때
+window.addEventListener("online", handleWindowOnline);      /// wifi 켜졌을때
