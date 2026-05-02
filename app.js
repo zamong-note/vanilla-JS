b52ae90a4fe9701b02e202df8ed3a70544471612
zamong-note <tamongnote@naver.com>
2026-05-02 20:34:01 +0900

    #3.8 CSS in Javascript part Three

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const h1 = document.querySelector("div.title:first-child h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked"
-    if (h1.className === clickedClass) {  /// className은 getter이자 setter이다
-        h1.className = "";
-    } else {
-        h1.className = clickedClass;
-    } 
+    h1.classList.toggle("clicked");
 }
 
 
 h1.addEventListener("click", handleTitleClick);  
+
+
+
+/* line 4 한 줄은 아래 코드와 같다
+
+function handleTitleClick() {
+    const clickedClass = "clicked"
+    if (h1.classList.contains(clickedClass)) {  /// html 클래스에 포함되어있는지 확인
+        h1.classList.remove(clickedClass);
+    } else {
+        h1.classList.add(clickedClass);
+    } 
+}
+ */
