60b6eb27dc00e1f762e3d1e718d5d7432302cde9
zamong-note <tamongnote@naver.com>
2026-05-02 19:42:21 +0900

    #3.4 Events part Two

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ function handleTitleClick() {
     title.style.color = "blue";
 }
 
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);  /// 클릭하는걸 리스닝하고 있음. 발생하면 function이 동작하게 한다. 
+title.addEventListener("mouseenter", handleMouseEnter);   /// 마우스가 올라갔을 때
+title.addEventListener("mouseleave", handleMouseLeave);   /// 마우스가 떠났을 때
+
