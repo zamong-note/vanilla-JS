b102dc5ced1c3cd7788d416a6ead6c98f286d707
zamong-note <tamongnote@naver.com>
2026-05-02 17:43:09 +0900

    #3.3 Events

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("hi");
+const title = document.querySelector("div.title:first-child h1");
+
+function handleTitleClick() {
+	console.log("title was clicked!");
+    title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);  /// 클릭하는걸 리스닝하고 있음. 발생하면 function이 동작하게 한다. 
