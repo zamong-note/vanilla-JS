3203d8fc68e8a81de809b3d2fb9e72622eb736ea
zamong-note <tamongnote@naver.com>
2026-05-03 20:03:21 +0900

    #7.3 Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -1,30 +1,39 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");  // document.querySelector("#todo-form input"); 과 동일한 코드이다. 
+const toDoInput = toDoForm.querySelector("input");  
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];   // 배열을 하나 만들어주기. newTodo가 만들어질때마다 array에 push할것
+
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(toDos));   // 배열처럼 보이게 출력할 수 있다
+}
+
+
 function deleteTodo(event){
-    // console.log(event.target.parentElement.innerText);   -> 어떤 list인지 텍스트 출력
     const li = event.target.parentElement;
     li.remove();
 }
 
 function paintToDo(newTodo){
-    const li = document.createElement("li");   // li를 추가함. 괄호안은 html 태그이름이어야하고 변수명은 상관x
-    const span = document.createElement("span");    // span을 추가함.
+    const li = document.createElement("li");   
+    const span = document.createElement("span");   
     const button = document.createElement("button");
     span.innerText = newTodo;
     button.innerText = "X";
     button.addEventListener("click", deleteTodo);
-    li.appendChild(span);           // html에 실제로 span이 추가됨. append는 마지막에들어감
-    li.appendChild(button);         // span이 생길 때 버튼도 같이 생기게 하고 싶다
-    toDoList.appendChild(li);       // 실제로 html에 보이도록 해주는 코드
+    li.appendChild(span);           
+    li.appendChild(button);         
+    toDoList.appendChild(li);      
 }
 
 function handleToDoSubmit() {
     event.preventDefault();
     const newTodo = toDoInput.value;
-    toDoInput.value = "";     // 엔터 칠때마다 창을 비우기
-    paintToDo(newTodo);     // 함수 종료 전 paintToDo에 newTodo 값 집어넣고 호출
+    toDoInput.value = "";    
+    toDos.push(newTodo);    // newTodo가 만들어질때마다 array에 push하기
+    paintToDo(newTodo);     
+    saveToDos();        // 저장하는 함수 호출하기
 }
 
 
@@ -32,4 +41,4 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 
 
-// 현재 코드에서는 새로고침하면 다 사라지고, 삭제도 불가하다
+// 아직 새로고침하면 다 사라진다
