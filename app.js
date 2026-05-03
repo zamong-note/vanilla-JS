0ac0e2360425abe226afeba65ae9b1080594f402
zamong-note <tamongnote@naver.com>
2026-05-03 10:42:40 +0900

    #4.6 Loading Username

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const loginForm = document.querySelector(".login-form");
-const loginInput = document.querySelector(".login-form input");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"  // string만 포함된 변수는 대문자로 표기하는 관행이 있음
+const USERNAME_KEY = "username"
 
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    localStorage.setItem("username", username);    // key와 value
-    greeting.innerText = `Hello ${username}`;      
+    localStorage.setItem(USERNAME_KEY, username);    // key와 value
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
