5c3f0ebe6a5224bf83b716f7d7afdde1dd4f4c2a
zamong-note <tamongnote@naver.com>
2026-05-03 19:49:58 +0900

    #7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");  // document.querySelector("#todo-form input"); 과 동일한 코드이다. 
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event){
+    // console.log(event.target.parentElement.innerText);   -> 어떤 list인지 텍스트 출력
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");   // li를 추가함. 괄호안은 html 태그이름이어야하고 변수명은 상관x
     const span = document.createElement("span");    // span을 추가함.
-    li.appendChild(span);           // html에 실제로 span이 추가됨
+    const button = document.createElement("button");
     span.innerText = newTodo;
+    button.innerText = "X";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);           // html에 실제로 span이 추가됨. append는 마지막에들어감
+    li.appendChild(button);         // span이 생길 때 버튼도 같이 생기게 하고 싶다
     toDoList.appendChild(li);       // 실제로 html에 보이도록 해주는 코드
 }
 
