1659ebb62a8223857aafa24376da9dd22a36fbd1
zamong-note <tamongnote@naver.com>
2026-05-03 10:05:10 +0900

    #4.4 Getting Username

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit(tomato) {
-    tomato.preventDefault();
-    console.log(tomato);
-    console.log(loginInput.value);
-}
+const HIDDEN_CLASSNAME = "hidden"  // string만 포함된 변수는 대문자로 표기하는 관행이 있음
 
-function handleLinkClick(event) {
+
+function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+    greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;       // 위 코드와 같은 동작을 수행하는데 이게 더 보기 좋다
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
