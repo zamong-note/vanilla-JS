71a3f261080935d7e9dbb8f3d7db3029173fb4bb
zamong-note <tamongnote@naver.com>
2026-05-03 18:54:03 +0900

    #5.2 PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,8 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-getClock()     // 한번 실행 후에 매 초마다 실행
-setTimeout(getClock, 1000);
+getClock();     // 한번 실행 후에 매 초마다 실행
+setInterval(getClock, 1000);
