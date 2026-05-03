12d4cea479d7f8fb2948a407e501d0969f5d0752
zamong-note <tamongnote@naver.com>
2026-05-03 09:14:01 +0900

    #4.1 Form Submission

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-/* 
-const loginForm = document.querySelector(".login-form"); /// 또는 getElementById("login-form")
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-*/
-
 const loginInput = document.querySelector(".login-form input");
 const loginButton = document.querySelector(".login-form button");
 
 function onLoginBtnClick() {
-    console.log("hello", loginInput.value);
+    const username = loginInput.value;
+/*   if(username === ""){
+        alert("이름을 입력하세요");
+    } else if(username.length > 15) {    /// string 다음 lenght만 하면 되도록 js에서 사전 구현
+        alert("너무 길어요")
+    }*/
 }
 
 
-loginButton.addEventListener("click", onLoginBtnClick)
 
-
-
-
-
-
+loginButton.addEventListener("click", onLoginBtnClick);
