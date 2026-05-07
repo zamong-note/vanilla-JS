dd938a3354a6bced7988badfd569edaf26017a1c
zamong-note <tamongnote@naver.com>
2026-05-07 22:13:07 +0900

    #day17~18

--- js/weather.js
+++ js/weather.js
@@ -11,7 +11,7 @@ function onGeoOk(position) {
         const weather = document.querySelector("#weather span:first-child");
         const city = document.querySelector("#weather span:last-child");
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}도`;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
     });
 }
 function onGeoError() {
